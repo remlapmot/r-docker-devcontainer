ac9d0dc9a748b148b73ddaa13c2e850923e228d7
Tom Palmer <remlapmot@hotmail.com>
2023-07-09 16:01:47 +0100

    Reformat if statements

--- setup.R
+++ setup.R
@@ -1,7 +1,35 @@
-if (!requireNamespace("collections", quietly = TRUE)) install.packages("./cran/__linux__/focal/collections_0.3.7.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
-if (!requireNamespace("languageserver", quietly = TRUE)) install.packages("./cran/__linux__/focal/languageserver_0.3.15.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
-if (!requireNamespace("systemfonts", quietly = TRUE)) install.packages("./cran/__linux__/focal/systemfonts_1.0.4.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
-if (!requireNamespace("httpgd", quietly = TRUE)) install.packages("./cran/__linux__/focal/httpgd_1.3.1.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
-if (!requireNamespace("lintr", quietly = TRUE)) install.packages("./cran/__linux__/focal/lintr_3.0.2.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
-if (!requireNamespace("rex", quietly = TRUE)) install.packages("./cran/__linux__/focal/rex_1.2.1.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
-if (!requireNamespace("cyclocomp", quietly = TRUE)) install.packages("./cran/__linux__/focal/cyclocomp_1.1.0.tar.gz", repos = NULL, lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+if (!requireNamespace("collections", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/collections_0.3.7.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
+if (!requireNamespace("languageserver", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/languageserver_0.3.15.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
+if (!requireNamespace("systemfonts", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/systemfonts_1.0.4.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
+if (!requireNamespace("httpgd", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/httpgd_1.3.1.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
+if (!requireNamespace("lintr", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/lintr_3.0.2.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
+if (!requireNamespace("rex", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/rex_1.2.1.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
+if (!requireNamespace("cyclocomp", quietly = TRUE)) {
+  install.packages("./cran/__linux__/focal/cyclocomp_1.1.0.tar.gz",
+    repos = NULL,
+    lib = "/renv/lib/R-4.0/x86_64-pc-linux-gnu")
+}
