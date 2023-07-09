6f3b4515fac3825c58db45686a8f8762f20d9156
Tom Palmer <remlapmot@hotmail.com>
2023-07-09 07:01:42 +0100

    Don't load packages

--- setup.R
+++ setup.R
@@ -2,6 +2,3 @@ if (!requireNamespace('collections', quietly = TRUE)) install.packages('./cran/_
 if (!requireNamespace('languageserver', quietly = TRUE)) install.packages('./cran/__linux__/focal/languageserver_0.3.15.tar.gz', repos = NULL)
 if (!requireNamespace('systemfonts', quietly = TRUE)) install.packages('./cran/__linux__/focal/systemfonts_1.0.4.tar.gz', repos = NULL)
 if (!requireNamespace('httpgd', quietly = TRUE)) install.packages('./cran/__linux__/focal/httpgd_1.3.1.tar.gz', repos = NULL)
-
-if (requireNamespace('languageserver', quietly = TRUE)) library(languageserver)
-if (requireNamespace('httpgd', quietly = TRUE)) library(httpgd)
